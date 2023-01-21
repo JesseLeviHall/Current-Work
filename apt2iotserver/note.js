b89057a964fe87850febe680c85ef07cccf08da9
jesse h <jesselevihall@gmail.com>
2023-01-20 20:58:43 -0700

    ingress security groups

--- apt2iotserver/note.js
+++ apt2iotserver/note.js
@@ -1,12 +1,3 @@
-const API = axios.create({
-	baseURL: 'http://192.168.0.55:3500',
-	timeout: 5000,
-	responseType: 'json',
-	withCredentials: true,
-	headers: {
-		Authorization: `Bearer ${await AsyncStorage.getItem('token')}`,
-	},
-});
 /* 
 HTTPS: configure cloudfront on aws or,
 to create a new self signed ssl:
