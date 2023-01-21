2759efaa3a3b4d1b9a3b9e8317b74abef1fa6bb9
jesse h <jesselevihall@gmail.com>
2023-01-20 20:56:47 -0700

    docker file

--- apt2iotserver/note.js
+++ apt2iotserver/note.js
@@ -7,3 +7,15 @@ const API = axios.create({
 		Authorization: `Bearer ${await AsyncStorage.getItem('token')}`,
 	},
 });
+/* 
+
+starting new ec2:
+sudo yum update -y
+sudo yum install docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+restart connection
+docker login, enter username and password
+docker run -it -p 3500:3500 apt2iot/backend<version>
+
+*/
