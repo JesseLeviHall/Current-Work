44896dcf89f8b158960e1a7958785a907b868095
jesse h <jesselevihall@gmail.com>
2023-01-20 20:58:20 -0700

    image build

--- apt2iotserver/note.js
+++ apt2iotserver/note.js
@@ -8,6 +8,20 @@ const API = axios.create({
 	},
 });
 /* 
+HTTPS: configure cloudfront on aws or,
+to create a new self signed ssl:
+openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365
+try chmod 400 server.key.pem
+
+aws instance connect ingress ip:
+18.206.107.24/29
+
+docker build . -t apt2iot/backend<version>
+docker run -it -p 3500:3500 apt2iot/backend<version>
+    //only for testing build - DB will not connect
+docker push apt2iot/backend<version>
+
+
 starting new ec2:
 sudo yum update -y
 sudo yum install docker
