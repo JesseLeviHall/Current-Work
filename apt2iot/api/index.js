1aa6dfac103a5e216867d2a97e29dc02616bd26e
jesse h <jesselevihall@gmail.com>
2023-01-22 09:46:27 -0700

    connect to ec2

--- apt2iot/api/index.js
+++ apt2iot/api/index.js
@@ -4,7 +4,7 @@ import jdubDecode from 'jwt-decode';
 
 // set axios configurations
 const API = axios.create({
-	baseURL: 'http://192.168.0.55:3500',
+	baseURL: 'http://52.87.229.12:3500',
 	timeout: 15000,
 	withCredentials: false,
 });
