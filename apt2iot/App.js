d9ad2a943df79ac45d5e2770a0541b37fe3800c5
jesse h <jesselevihall@gmail.com>
2023-02-19 10:56:00 -0700

    intl date formats

--- apt2iot/App.js
+++ apt2iot/App.js
@@ -5,6 +5,13 @@ import { AuthProvider } from './hooks/useAuth';
 import { HomeScreenProvider } from './hooks/useHomeComponent';
 import { en, registerTranslation } from 'react-native-paper-dates';
 registerTranslation('en', en);
+import 'intl';
+import 'intl/locale-data/jsonp/en'; // or whichever locale(s) you need
+
+// initialize the polyfill
+if (typeof Intl.__disableRegExpRestore === 'function') {
+	Intl.__disableRegExpRestore();
+}
 
 export default function App() {
 	return (
