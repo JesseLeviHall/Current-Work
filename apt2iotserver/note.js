d493041400a059732585b1ff29445398dd1e37de
jesse h <jesselevihall@gmail.com>
2023-02-22 20:35:59 -0700

    edit note

--- apt2iotserver/note.js
+++ apt2iotserver/note.js
@@ -4,21 +4,7 @@ to create a new self signed ssl:
 openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365
 try chmod 400 server.key.pem
 
-aws instance connect ingress ip:
-18.206.107.24/29
-
-docker build . -t apt2iot/backend<version>
-docker run -it -p 3500:3500 apt2iot/backend<version>
-    //only for testing build - DB will not connect
-docker push apt2iot/backend<version>
 
 
-starting new ec2:
-sudo yum update -y
-sudo yum install docker
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-restart connection
-docker login, enter username and password
-docker run -it -p 3500:3500 apt2iot/backend<version>
+
 */
