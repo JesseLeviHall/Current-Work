45b7a94e9ce931ae9d31ce6d2fe37bc2debf1c1b
jesse h <jesselevihall@gmail.com>
2023-03-01 09:26:55 -0700

    chmod 400

--- apt2iotserver/note.js
+++ apt2iotserver/note.js
@@ -2,6 +2,6 @@
 HTTPS: configured cloudfront on aws with load balancer
 or,to create a new self signed ssl:
 openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365
-try chmod 400 server.key.pem
+chmod 400 server.key.pem
 
 */
