fada31e108f54c6465f105a9b9eff90d7717e582
jesse h <jesselevihall@gmail.com>
2023-03-01 09:27:50 -0700

    qr code

--- apt2iotserver/note.js
+++ apt2iotserver/note.js
@@ -4,4 +4,8 @@ or,to create a new self signed ssl:
 openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365
 chmod 400 server.key.pem
 
+
+Next, redesign bottom bar, and implement qr code functionality
+
+
 */
