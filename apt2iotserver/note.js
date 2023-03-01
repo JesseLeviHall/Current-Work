f67366b70e00568ba1cd820a043943ae4c6df2d6
jesse h <jesselevihall@gmail.com>
2023-02-28 20:34:47 -0700

    https for production

--- apt2iotserver/note.js
+++ apt2iotserver/note.js
@@ -1,6 +1,6 @@
 /* 
-HTTPS: configure cloudfront on aws or,
-to create a new self signed ssl:
+HTTPS: configured cloudfront on aws with load balancer
+or,to create a new self signed ssl:
 openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365
 try chmod 400 server.key.pem
 
